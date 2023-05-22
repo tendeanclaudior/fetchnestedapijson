b479b08ba8a738cd42308121bd6632bc09b604ab
Dio Tendean <tendeanclaudio1234@gmail.com>
2023-05-22 21:56:59 +0800

    Initial Commit

---
+++ FetchNestedApiJson/FetchNestedApiJsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchNestedApiJsonApp.swift
+//  FetchNestedApiJson
+//
+//  Created by Claudio Tendean on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchNestedApiJsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
